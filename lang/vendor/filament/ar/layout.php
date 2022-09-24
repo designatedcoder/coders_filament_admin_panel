98a569045d69f11cf01003b002cc1ef1ac7bfdf3
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2022-09-24 19:51:20 -0400

    installed filament with translations and config

---
+++ lang/vendor/filament/ar/layout.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'direction' => 'rtl',
+
+    'buttons' => [
+
+        'dark_mode' => [
+            'label' => 'تفعيل الوضع الليلي',
+        ],
+
+        'database_notifications' => [
+            'label' => 'عرض التنبيهات',
+        ],
+
+        'light_mode' => [
+            'label' => 'تفعيل وضع النهار',
+        ],
+
+        'logout' => [
+            'label' => 'تسجيل الخروج',
+        ],
+
+    ],
+
+];
