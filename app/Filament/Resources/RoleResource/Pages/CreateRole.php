cda2bcf4fb5f7a16310513e46ee3faf07fd66ae5
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2022-10-07 21:30:07 -0400

    Created Role Resource with Relation Manager

---
+++ app/Filament/Resources/RoleResource/Pages/CreateRole.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\RoleResource\Pages;
+
+use App\Filament\Resources\RoleResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRole extends CreateRecord
+{
+    protected static string $resource = RoleResource::class;
+
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Filament/Resources/RoleResource/Pages/EditRole.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\RoleResource\Pages;
+
+use App\Filament\Resources\RoleResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRole extends EditRecord
+{
+    protected static string $resource = RoleResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+}
