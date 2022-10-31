dd44031331f0d745051d9ebd2957dc1ded3a08e4
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2022-10-30 20:15:54 -0400

    custom filament widgets

---
+++ app/Filament/Widgets/UsersChart.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Carbon\Carbon;
+use App\Models\User;
+use Filament\Widgets\BarChartWidget;
+
+class UsersChart extends BarChartWidget
+{
+    protected static ?string $heading = 'Users';
+
+    protected function getData(): array {
+        $users = User::select('created_at')->get()->groupBy(function($users) {
+            return Carbon::parse($users->created_at)->format('F');
+        });
+        $quantities = [];
+        foreach ($users as $user => $value) {
+            array_push($quantities, $value->count());
+        }
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Users Joined',
+                    'data' => $quantities,
+                    'backgroundColor' => [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    'borderColor' => [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+                    'borderWidth' => 1
+                ],
+            ],
+            'labels' => $users->keys(),
+        ];
+    }
+}
